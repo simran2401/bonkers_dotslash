5312d655bee18f3c6706218d443e736306081f81
Simran Shah <simransah2401@gmail.com>
2021-12-29 15:29:57 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+const { products } = require('../data');
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+  res.send('Hello World');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
 
 var server = app.listen(8000, function () {
-    var port = server.address().port
-    console.log("Server running at http://localhost:%s", port)
-})
+  var port = server.address().port;
+  console.log('Server running at http://localhost:%s', port);
+});
